314e5d7f9db089f29317b7f80c6e5dde2371f89d
Kevin Polangco <122111588+polangco@users.noreply.github.com>
2025-10-02 10:51:29 -0400

    Auto-update: Shopify customer profiles and purchase behavior analysis

--- ad_hoc/LOVB_2171_past_ticket_purchasers/LOVB-2171 past ticket purchasers.sql
+++ ad_hoc/LOVB_2171_past_ticket_purchasers/LOVB-2171 past ticket purchasers.sql
@@ -6,14 +6,14 @@ with _select as (
 		phone_number,
 		address_city, 
 		address_state,
-		event_code, 
-		match_date,
-		extract(week from match_date) as match_week
-	from prod_intermediate.int_single_ticket_purchase
+		order_id, 
+		order_date,
+		extract(week from order_date) as order_week
+	from prod_intermediate.int_shopify_orders
 	where coalesce(first_name, last_name, '') != ''
 ), 
 
-_adj_match_week as (
+_adj_order_week as (
 	select 
 		first_name,
 		last_name, 
@@ -21,9 +21,9 @@ _adj_match_week as (
 		phone_number,
 		address_city, 
 		address_state,
-		event_code, 
-		match_date,
-		match_week - (select min(match_week) from _select) + 1 as match_week
+		order_id, 
+		order_date,
+		order_week - (select min(order_week) from _select) + 1 as order_week
 	from _select
 ), 
 
@@ -35,7 +35,7 @@ _rank as (
 		phone_number,
 		address_city, 
 		address_state,
-		row_number() over(partition by email order by last_name, first_name, phone_number, match_week) as row_id
+		row_number() over(partition by email order by last_name, first_name, phone_number, order_week) as row_id
 	from _select
 ), 
 
@@ -55,20 +55,20 @@ _unique as (
 _email_weeks as (
 
 	select email, 
-		max(case when match_week = 1 then 1 else 0 end) :: BOOLEAN as week_1,
-		max(case when match_week = 2 then 1 else 0 end) :: BOOLEAN as week_2,
-		max(case when match_week = 3 then 1 else 0 end) :: BOOLEAN as week_3,
-		max(case when match_week = 4 then 1 else 0 end) :: BOOLEAN as week_4,
-		max(case when match_week = 5 then 1 else 0 end) :: BOOLEAN as week_5,
-		max(case when match_week = 6 then 1 else 0 end) :: BOOLEAN as week_6,
-		max(case when match_week = 7 then 1 else 0 end) :: BOOLEAN as week_7,
-		max(case when match_week = 8 then 1 else 0 end) :: BOOLEAN as week_8,
-		max(case when match_week = 9 then 1 else 0 end) :: BOOLEAN as week_9,
-		max(case when match_week = 10 then 1 else 0 end) :: BOOLEAN as week_10,
-		max(case when match_week = 11 then 1 else 0 end) :: BOOLEAN as week_11,
-		max(case when match_week = 12 then 1 else 0 end) :: BOOLEAN as week_12,
-		max(case when match_week = 13 then 1 else 0 end) :: BOOLEAN as week_13
-	from _adj_match_week
+		max(case when order_week = 1 then 1 else 0 end) :: BOOLEAN as week_1,
+		max(case when order_week = 2 then 1 else 0 end) :: BOOLEAN as week_2,
+		max(case when order_week = 3 then 1 else 0 end) :: BOOLEAN as week_3,
+		max(case when order_week = 4 then 1 else 0 end) :: BOOLEAN as week_4,
+		max(case when order_week = 5 then 1 else 0 end) :: BOOLEAN as week_5,
+		max(case when order_week = 6 then 1 else 0 end) :: BOOLEAN as week_6,
+		max(case when order_week = 7 then 1 else 0 end) :: BOOLEAN as week_7,
+		max(case when order_week = 8 then 1 else 0 end) :: BOOLEAN as week_8,
+		max(case when order_week = 9 then 1 else 0 end) :: BOOLEAN as week_9,
+		max(case when order_week = 10 then 1 else 0 end) :: BOOLEAN as week_10,
+		max(case when order_week = 11 then 1 else 0 end) :: BOOLEAN as week_11,
+		max(case when order_week = 12 then 1 else 0 end) :: BOOLEAN as week_12,
+		max(case when order_week = 13 then 1 else 0 end) :: BOOLEAN as week_13
+	from _adj_order_week
 	group by email
 ),
 
@@ -100,5 +100,3 @@ _final as (
 )
 
 select * from _final order by address_state, address_city, last_name;
-;
-
